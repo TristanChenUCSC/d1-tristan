0068d9cb385db6fca8075ecbd9522548afafd2db
TristanChenUCSC <trzchen@ucsc.edu>
2025-10-07 21:04:32 +0000

    Step 1 Complete: Added a Clickable Button

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Example change: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const button = document.createElement("button");
+button.textContent = "💵";
+document.body.append(button);
