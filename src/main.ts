aae30ba74a2c44045d1cc41b795ff2874d93c681
TristanChenUCSC <trzchen@ucsc.edu>
2025-10-10 03:23:17 +0000

    Added a counter element and counter logic + changed emoji to 🧠 (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,17 @@ document.body.innerHTML = `
 
 // Create Button
 const button = document.createElement("button");
-button.textContent = "💰";
+button.textContent = "🧠";
 document.body.append(button);
+
+// Create counter element
+const counterElement = document.createElement("div");
+counterElement.textContent = "0 Brain Nourishment Points";
+document.body.append(counterElement);
+
+// Counter logic
+let counter: number = 0;
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.textContent = `${counter} Brain Nourishment Points`;
+});
